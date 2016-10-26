5a148e38a989bd3f26afbd4f0b9449be033e3845
Gokul Reddy <gokul.evuri@ericsson.com>
2016-10-26 13:41:12 +0200

    visual sugar for builder

--- Java/com/gildedrose/GildedRoseTest.java
+++ Java/com/gildedrose/GildedRoseTest.java
@@ -20,7 +20,11 @@ public class GildedRoseTest {
     @Test
     public void eachUpdateDecreasesQualityValue()
     {
-        myRose = new GildedRoseItemsBuilder().name("foo").quality(10).sellIn(2).update();
+        myRose = new GildedRoseItemsBuilder()
+                .name("foo")
+                .quality(10)
+                .sellIn(2)
+                .update();
 
         assertEquals(9, myRose.qualityFor(0));
         assertEquals(1, myRose.sellInFor(0));
@@ -29,14 +33,23 @@ public class GildedRoseTest {
     @Test
     public void eachUpdateDecreasesSellInValue()
     {
-        myRose = new GildedRoseItemsBuilder().name("foo").quality(9).sellIn(1).update();
+        myRose = new GildedRoseItemsBuilder()
+                .name("foo")
+                .quality(9)
+                .sellIn(1)
+                .update();
+
         assertEquals(0, myRose.sellInFor(0));
     }
 
     @Test
     public void eachUpdateDecreasesQualityTwiceAsFastWhenExpired()
     {
-        myRose = new GildedRoseItemsBuilder().name("foo").quality(8).sellIn(0).update();
+        myRose = new GildedRoseItemsBuilder()
+                .name("foo")
+                .quality(8)
+                .sellIn(0)
+                .update();
 
         assertEquals(6, myRose.qualityFor(0));
         assertEquals(0, myRose.sellInFor(0));
@@ -45,7 +58,11 @@ public class GildedRoseTest {
     @Test
     public void qualityOfAnItemIsNeverNegativeWithQuality1()
     {
-        myRose = new GildedRoseItemsBuilder().name("foo").quality(1).sellIn(0).update();
+        myRose = new GildedRoseItemsBuilder()
+                .name("foo")
+                .quality(1)
+                .sellIn(0)
+                .update();
 
         assertEquals(myRose.qualityFor(0) >= 0, true);
         assertEquals(0, myRose.sellInFor(0));
@@ -54,7 +71,11 @@ public class GildedRoseTest {
     @Test
     public void qualityOfAnItemIsNeverNegativeWithQuality2()
     {
-        myRose = new GildedRoseItemsBuilder().name("foo").quality(2).sellIn(0).update();
+        myRose = new GildedRoseItemsBuilder()
+                .name("foo")
+                .quality(2)
+                .sellIn(0)
+                .update();
 
         assertEquals(myRose.qualityFor(0) >= 0, true);
         assertEquals(0, myRose.sellInFor(0));
@@ -63,7 +84,11 @@ public class GildedRoseTest {
     @Test
     public void qualityOfAnItemIsNeverNegative()
     {
-        myRose = new GildedRoseItemsBuilder().name("foo").quality(0).sellIn(0).update();
+        myRose = new GildedRoseItemsBuilder()
+                .name("foo")
+                .quality(0)
+                .sellIn(0)
+                .update();
 
         assertEquals(myRose.qualityFor(0) >= 0, true);
         assertEquals(0, myRose.sellInFor(0));
@@ -72,7 +97,11 @@ public class GildedRoseTest {
     @Test
     public void eachUpdateIncreasesQualityOfAgedBrie()
     {
-        myRose = new GildedRoseItemsBuilder().name("Aged Brie").quality(8).sellIn(9).update();
+        myRose = new GildedRoseItemsBuilder()
+                .name("Aged Brie")
+                .quality(8)
+                .sellIn(9)
+                .update();
 
         assertEquals(9, myRose.qualityFor(0));
         assertEquals(8, myRose.sellInFor(0));
@@ -83,7 +112,11 @@ public class GildedRoseTest {
     @Test
     public void qualityOfAnItemIsNeverMoreThan50()
     {
-        myRose = new GildedRoseItemsBuilder().name("Aged Brie").quality(50).sellIn(9).update();
+        myRose = new GildedRoseItemsBuilder()
+                .name("Aged Brie")
+                .quality(50)
+                .sellIn(9)
+                .update();
 
         assertEquals(50, myRose.qualityFor(0));
         assertEquals(8, myRose.sellInFor(0));
@@ -92,7 +125,11 @@ public class GildedRoseTest {
     @Test
     public void qualityOfSulfurasNeverChanges()
     {
-        myRose = new GildedRoseItemsBuilder().name("Sulphurs, Hand of Ragnaros").quality(70).sellIn(0).update();
+        myRose = new GildedRoseItemsBuilder()
+                .name("Sulphurs, Hand of Ragnaros")
+                .quality(70)
+                .sellIn(0)
+                .update();
 
         assertEquals(70, myRose.qualityFor(0));
         assertEquals(0, myRose.sellInFor(0));
@@ -101,7 +138,11 @@ public class GildedRoseTest {
     @Test
     public void sellInOfSulfurasNeverChanges()
     {
-        myRose = new GildedRoseItemsBuilder().name("Sulphurs, Hand of Ragnaros").quality(80).sellIn(0).update();
+        myRose = new GildedRoseItemsBuilder()
+                .name("Sulphurs, Hand of Ragnaros")
+                .quality(80)
+                .sellIn(0)
+                .update();
 
         assertEquals(80, myRose.qualityFor(0));
         assertEquals(0, myRose.sellInFor(0));
@@ -110,7 +151,11 @@ public class GildedRoseTest {
     @Test
     public void qualityOfBSPassesIncreasesBy2Before10DaysSellInDate()
     {
-        myRose = new GildedRoseItemsBuilder().name("Backstage passes to a TAFKAL80ETC concert").quality(10).sellIn(10).update();
+        myRose = new GildedRoseItemsBuilder()
+                .name("Backstage passes to a TAFKAL80ETC concert")
+                .quality(10)
+                .sellIn(10)
+                .update();
 
         assertEquals(12, myRose.qualityFor(0));
         assertEquals(9, myRose.sellInFor(0));
@@ -119,7 +164,11 @@ public class GildedRoseTest {
     @Test
     public void qualityOfBSPassesIncreasesBy3Before5DaysSellInDate()
     {
-        myRose = new GildedRoseItemsBuilder().name("Backstage passes to a TAFKAL80ETC concert").quality(10).sellIn(5).update();
+        myRose = new GildedRoseItemsBuilder()
+                .name("Backstage passes to a TAFKAL80ETC concert")
+                .quality(10)
+                .sellIn(5)
+                .update();
 
         assertEquals(13, myRose.qualityFor(0));
         assertEquals(4, myRose.sellInFor(0));
@@ -128,7 +177,11 @@ public class GildedRoseTest {
     @Test
     public void qualityOfBSPassesDrops0AfterSellInDate()
     {
-        myRose = new GildedRoseItemsBuilder().name("Backstage passes to a TAFKAL80ETC concert").quality(10).sellIn(0).update();
+        myRose = new GildedRoseItemsBuilder()
+                .name("Backstage passes to a TAFKAL80ETC concert")
+                .quality(10)
+                .sellIn(0)
+                .update();
 
         assertEquals(0, myRose.qualityFor(0));
         assertEquals(0, myRose.sellInFor(0));
@@ -137,7 +190,11 @@ public class GildedRoseTest {
     @Test
     public void qualityOfConjuredDegradeBy2()
     {
-        myRose = new GildedRoseItemsBuilder().name("Conjured Mana Cake").quality(10).sellIn(10).update();
+        myRose = new GildedRoseItemsBuilder()
+                .name("Conjured Mana Cake")
+                .quality(10)
+                .sellIn(10)
+                .update();
 
         assertEquals(8, myRose.qualityFor(0));
         assertEquals(9, myRose.sellInFor(0));
@@ -146,7 +203,11 @@ public class GildedRoseTest {
     @Test
     public void qualityOfConjuredDegradeBy4WhenSellInIs0()
     {
-        myRose = new GildedRoseItemsBuilder().name("Conjured Mana Cake").quality(10).sellIn(0).update();
+        myRose = new GildedRoseItemsBuilder()
+                .name("Conjured Mana Cake")
+                .quality(10)
+                .sellIn(0)
+                .update();
 
         assertEquals(6, myRose.qualityFor(0));
         assertEquals(0, myRose.sellInFor(0));
